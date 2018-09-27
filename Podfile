76f38b45f4567ec2ae13cbb8879d23a2ba9922d2
reza pahlevi <levi@staff.gramedia.com>
2018-09-28 05:22:50 +0700

    initial commit

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+use_frameworks!
+def shared_pods
+    pod 'Alamofire'
+    pod 'SDWebImage', '~> 4.0'
+end
+
+target 'Search' do
+    shared_pods
+end
+
+target 'SearchTests' do
+    shared_pods
+end
+
+target 'SearchUITests' do
+    shared_pods
+end
---
+++ Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Search
+//
+//  Created by reza pahlevi on 9/28/18.
+//  Copyright © 2018 tokopedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
